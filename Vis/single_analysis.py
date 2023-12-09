8722f869617186c35819d4d0552f06d7c29157ff
tza <bienlor.163.com>
2023-12-09 21:59:20 +0800

    discrepency done

--- Vis/single_analysis.py
+++ Vis/single_analysis.py
@@ -34,7 +34,8 @@ for df in df_list:
 
 # Visualize the publication count by university for each file 
 for i in range( 
-    len(df_list)
+    # len(df_list)
+    0
     ):
 
     region = df_list[i]['region'][0]
@@ -65,17 +66,20 @@ for i in range(
 
 # Visualize the faculty member count by university for each file
 for i in range( 
-    len(df_list)):
+    0):
+    # len(df_list)):
 
     region = df_list[i]['region'][0]
     field = df_list[i]['field'][0]
 
+    df_sorted = df_list[i].sort_values('Faculty Count', ascending=False)
+
     plt.figure(figsize=(12,12))
-    ax = sns.barplot(x='abbr', y='Faculty Count', data=df_list[i])
+    ax = sns.barplot(x='abbr', y='Faculty Count', data=df_sorted)
     plt.title('Faculty Member Count by University in ' + field + ' ' + region + ' Institutions')
 
     # Get the unique abbreviations and corresponding university names
-    abbr_names = df_list[i][['abbr', 'University']]
+    abbr_names = df_sorted[['abbr', 'University']]
 
     # Create a legend
     patches = [mpatches.Patch(color=ax.patches[i].get_facecolor(), label=label) for i, label in enumerate(abbr_names['University'])]
@@ -92,3 +96,76 @@ for i in range(
     plt.savefig('./images/single_analysis/facs/svgs/faculty_in_{}_{}.svg'.format(field, region), dpi=300)
 
     # plt.show()
+
+
+# Visualize the discrepancy between faculty member count and publication count by university for each file
+for i in range( 
+    # 2
+    len(df_list)
+    ):
+    region = df_list[i]['region'][0]
+    field = df_list[i]['field'][0]
+
+    colors = sns.color_palette('pastel')[0:len(df_list[i]['abbr'])]
+    
+    # standardize the faculty memeber count and publication count, with the expecatin and standard deviation of the faculty member count
+    df_list[i]['Standardized Faculty Count'] = (df_list[i]['Faculty Count'] - df_list[i]['Faculty Count'].mean()) / df_list[i]['Faculty Count'].std()
+
+    df_list[i]['Standardized Publication Count'] = (df_list[i]['Publication Count'] - df_list[i]['Publication Count'].mean()) / df_list[i]['Publication Count'].std()
+
+    # calculate the discrepancy
+    df_list[i]['Discrepancy'] = df_list[i]['Standardized Faculty Count'] - df_list[i]['Standardized Publication Count']
+
+    fig, ax1 = plt.subplots(figsize=(12,12))
+
+    # Create the bar chart
+    bars = ax1.bar(df_list[i]['abbr'], df_list[i]['Discrepancy'], color= colors)
+
+    # Create a second axes for the line graph
+    ax2 = ax1.twinx()
+
+
+    # Create the line graph
+    ax2.plot(df_list[i]['abbr'], df_list[i]['Standardized Faculty Count'], color='green', label='Standardized Pub Count')
+    ax2.plot(df_list[i]['abbr'], df_list[i]['Standardized Publication Count'], color='red', label='Standardized Faculty Count')
+
+    # Add labels and title
+    ax1.set_xlabel('abbr')
+    ax1.set_ylabel('Discrepancy: st_Fac -st_Pub', color='black')
+
+    ax2.set_ylabel('Count', color='black')
+    plt.title('Standardized Pub Count and Faculty Count by University in ' + field + ' ' + region + ' Institutions')
+
+    # Add a legend
+    # ax2.legend()
+    # handles = [plt.Rectangle((0,0),1,1, color=bar.get_facecolor()) for bar in bars]
+    # plt.legend(handles, df_list[i]['abbr'], title='University')
+
+    # Create a legend
+    abbr_names = df_list[i][['abbr', 'University']]
+    patches = [mpatches.Patch(color=ax1.patches[i].get_facecolor(), label=label) for i, label in enumerate(abbr_names['University'])]
+
+
+    # Add the exact number on top of each bar
+    # for p in ax.patches:
+    #     ax1.text(p.get_x() + p.get_width() / 2., p.get_height(), '%d' % int(p.get_height()), 
+    #             fontsize=12, color='black', ha='center', va='bottom')
+    
+    for p in ax1.patches:
+        height = float(p.get_height())
+        color = 'black' if height >= 0 else 'blue'
+        ax1.text(p.get_x() + p.get_width() / 2., height, '%4.3f' % height, 
+                fontsize=9, color=color, ha='center', va='bottom')
+
+    # Add a legend to the plot
+    ax1.legend(handles=patches, bbox_to_anchor=(0.99, 0.02), loc='lower right', borderaxespad=0., handlelength=0.5, prop={'size': 6}, ncol = 3)
+    ax2.legend(loc='upper right', handlelength=0.5, prop={'size': 7})
+
+    plt.savefig('./images/single_analysis/discrep/pngs/dif_in_{}_{}.png'.format(field, region), dpi=300)
+    plt.savefig('./images/single_analysis/discrep/svgs/dif_in_{}_{}.svg'.format(field, region), dpi=300)
+
+    df_list[i].to_csv('./Discrepancies/Discrepancy_{}_{}.csv'.format(field, region), index=False)
+
+    # Show the plot
+    # plt.show()
+    plt.close()
